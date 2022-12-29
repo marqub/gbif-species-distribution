ccfb367cd2f7c2af0f97c3a746369811ad4176e1
marqub <bmarquie@gmail.com>
2022-12-29 15:28:29 +0100

    init

---
+++ create_repo_structure.sh
@@ -0,0 +1,20 @@
+# Create the top-level directories
+mkdir data notebooks reports
+
+# Create the CSV files in the data directory
+touch data/gbif_data.csv
+touch data/new_data.csv
+
+# Create the Jupyter notebooks in the notebooks directory
+touch notebooks/phase-1-data-exploration.ipynb
+touch notebooks/phase-2-model-training.ipynb
+touch notebooks/phase-3-model-deployment-and-evaluation.ipynb
+
+# Create the Markdown files in the reports directory
+touch reports/phase-1-data-exploration.md
+touch reports/phase-2-model-training.md
+touch reports/phase-3-model-deployment-and-evaluation.md
+
+# Create the README.md and LICENSE files
+touch README.md
+touch LICENSE
